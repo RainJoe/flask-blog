772a324888852ffaf9e9d4650fa29fb2aa8be45b
RainJoe <1358137595@qq.com>
2018-08-14 20:27:39 +0800

    fix arg parse

--- app/resources/args.py
+++ app/resources/args.py
@@ -2,23 +2,23 @@ from flask_restful import reqparse
 
 # session args
 session_args = reqparse.RequestParser()
-session_args.add_argument("email", type=str)
-session_args.add_argument('password', type=str)
+session_args.add_argument("email", type=str, required=True)
+session_args.add_argument('password', type=str, required=True)
 
 # user args
 user_args = reqparse.RequestParser()
-user_args.add_argument('name', type=str)
-user_args.add_argument('email', type=str)
-user_args.add_argument('password', type=str)
+user_args.add_argument('name', type=str) 
+user_args.add_argument('email', type=str, required=True)
+user_args.add_argument('password', type=str, required=True)
 
 # article args
 article_args = reqparse.RequestParser()
-article_args.add_argument('title', type=str)
-article_args.add_argument('desc', type=str)
+article_args.add_argument('title', type=str, required=True)
+article_args.add_argument('desc', type=str, required=True)
 article_args.add_argument('img_id', type=str)
-article_args.add_argument('category', type=str)
-article_args.add_argument('body', type=str)
+article_args.add_argument('category', type=str, required=True)
+article_args.add_argument('body', type=str, required=True)
 
 # comment args
 comment_args = reqparse.RequestParser()
-comment_args.add_argument('body', type=str)
+comment_args.add_argument('body', type=str, required=True)
