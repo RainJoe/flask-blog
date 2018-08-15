d118e05e3a3920ad898b3f8fd9e7499711b3b30e
RainJoe <1358137595@qq.com>
2018-08-15 21:48:30 +0800

    fix arg parse type

--- app/resources/args.py
+++ app/resources/args.py
@@ -15,7 +15,7 @@ user_args.add_argument('password', type=str, required=True)
 article_args = reqparse.RequestParser()
 article_args.add_argument('title', type=str, required=True)
 article_args.add_argument('desc', type=str, required=True)
-article_args.add_argument('img_id', type=str)
+article_args.add_argument('img_id', type=int)
 article_args.add_argument('category', type=str, required=True)
 article_args.add_argument('body', type=str, required=True)
 
